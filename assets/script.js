587eb207ef268bf57ce71b5023cc794aca411fd3
Dinh Nguyen <nguyen_dinh282@yahoo.com>
2023-04-01 21:23:24 -0400

    add timer function, list other functions

---
+++ assets/script.js
@@ -0,0 +1,74 @@
+
+// .getElementById is a method that lets us quickly access element object id property that matches the string.
+var quizContainer = document.getElementById("container");
+var startBtn = document.getElementById("start-btn");
+// var result = document.getElementById("result"); 
+var question = document.getElementById("question");
+var choices = document.getElementById("choices");
+var timer = document.getElementById("timer");
+var hsContainer = document.getElementById("hs-container");
+var hsList = document.getElementById("hs-list")
+
+// variables in regards to the game
+var score = 0;
+var currentQuestIdx = 0;
+var timeRemaining = 90;
+// var timerInterval ; 
+var highScores = [];
+
+
+// addEventListener to listen for interactions from user (in this case, clicks) and then calls for a function.
+startBtn.addEventListener("click", startQuiz);
+
+//TODO add event listener forplay again button.
+
+
+function startQuiz() {
+// initiate game. reset variables to default.
+score = 0;
+currentQuestIdx = 0;
+timeRemaining = 90;
+
+// .classList allows us to add, remove, or replace classes. 
+startBtn.classList.add("hide");
+quizContainer.classList.remove("hide");
+hsContainer.classList.add("hide");
+
+// setInterval allows repeated calls of a function or executes a code snippet
+//with a fixed time delay. time is in milli second so a value of 1000 = 1 second.
+setInterval(countdownTimer, 1000);
+
+
+
+}
+
+
+function countdownTimer(){
+    timeRemaining--;
+    timer.innerText = `Time: ${timeRemaining} `
+    if(timeRemaining <= 0){
+        stopQuiz();
+    }
+
+}
+
+function showQuestions() {
+
+
+}
+
+
+function answerCheck() {
+
+}
+
+function stopQuiz() {
+
+
+}
+
+function showHighScore() {
+    //add code to pause game...
+
+
+}
