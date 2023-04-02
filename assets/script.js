ae1c9ad5088a06e0ff724777e168c3e1d6df06a5
Dinh Nguyen <nguyen_dinh282@yahoo.com>
2023-04-02 06:04:11 -0400

    add to style.css

--- assets/script.js
+++ assets/script.js
@@ -2,9 +2,11 @@
 // .getElementById is a method that lets us quickly access element object id property that matches the string.
 var quizContainer = document.getElementById("container");
 var startBtn = document.getElementById("start-btn");
+var playAgainBtn = document.getElementById("play-again-btn");
 // var result = document.getElementById("result"); 
-var question = document.getElementById("question");
-var choices = document.getElementById("choices");
+var question = document.getElementById("question"); // might not need if we store questions in an array.
+var choices = document.getElementById("choices"); //
+var answer = document.getElementById("choice")// 
 var timer = document.getElementById("timer");
 var hsContainer = document.getElementById("hs-container");
 var hsList = document.getElementById("hs-list")
@@ -16,11 +18,37 @@ var timeRemaining = 90;
 // var timerInterval ; 
 var highScores = [];
 
+var questions =[
+    {
+        question: "What is 1 + 1?",
+        options: ["0", "1", "2", "3"],
+        answer: "4",
+    },
+    {
+        question: "What is 1 + 1?",
+        options: ["0", "1", "2", "3"],
+        answer: "4",
+    },
+    {
+        question: "What is 1 + 1?",
+        options: ["0", "1", "2", "3"],
+        answer: "4",
+    },
+    {
+        question: "What is 1 + 1?",
+        options: ["0", "1", "2", "3"],
+        answer: "4",
+    }
+
+];
+
+
 
 // addEventListener to listen for interactions from user (in this case, clicks) and then calls for a function.
 startBtn.addEventListener("click", startQuiz);
+//TODO: have playAgainBtn hidden when startBtn isn't
+playAgainBtn.addEventListener('click', startQuiz);
 
-//TODO add event listener forplay again button.
 
 
 function startQuiz() {
@@ -31,6 +59,7 @@ timeRemaining = 90;
 
 // .classList allows us to add, remove, or replace classes. 
 startBtn.classList.add("hide");
+playAgainBtn.classList.add("hide");
 quizContainer.classList.remove("hide");
 hsContainer.classList.add("hide");
 
@@ -45,7 +74,7 @@ setInterval(countdownTimer, 1000);
 
 function countdownTimer(){
     timeRemaining--;
-    timer.innerText = `Time: ${timeRemaining} `
+    timer.innerText = `Time: ${timeRemaining}`;
     if(timeRemaining <= 0){
         stopQuiz();
     }
@@ -53,6 +82,9 @@ function countdownTimer(){
 }
 
 function showQuestions() {
+    var questionTitle = document.getElementById("quest-title");
+    questionTitle.textContent = questions[currentQuestIdx].question;
+    
 
 
 }
@@ -63,9 +95,14 @@ function answerCheck() {
 }
 
 function stopQuiz() {
-
-
+    //clearInterval is the opposite of setInterval. it stops the recurring calling of a function.
+ clearInterval(countdownTimer);
+ timer.innerText = `Time: 0`;
+ 
+ showHighScore();   
 }
+
+
 
 function showHighScore() {
     //add code to pause game...
